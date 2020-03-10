2c3cfd3475559f2591c22a18d4dcc539e845352d
werbower <werbower@gmail.com>
2020-03-10 17:52:48 +0200

    step first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'crisis-center', component: CrisisListComponent},
+  {path: 'heroes', component: HeroListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}), // only for demo
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
